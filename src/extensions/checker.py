fc3deead12c9594ddd1f2affefaa959599ca6019
Max Tetyushkin <maximtetuskin1267@gmail.com>
2025-10-12 20:54:59 +0700

    Add docstring to Checker class

--- src/extensions/checker.py
+++ src/extensions/checker.py
@@ -5,10 +5,18 @@ import pydantic
 
 
 class Checker:
+    """Handler class to validate data form.
+
+    It's used when api accepts data with content type `multipart/form-data`.
+    Use this type when endpoint accepts both json serializable and image data.
+
+    """
+
     def __init__(self, model: pydantic.BaseModel):
         self.model = model
 
-    def __call__(self, data: str = fastapi.Form(...)):
+    def __call__(self, data: str = fastapi.Form(...)) -> pydantic.BaseModel:
+        """Process data from form."""
         try:
             return self.model.model_validate_json(data)
         except pydantic.ValidationError as e:
