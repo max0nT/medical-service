6102308677644eda32db34f5e5a48d2163d3e40a
Max Tetyushkin <maksimtetuskin1267@gmail.com>
2026-01-23 16:25:10 +0700

    Setup scalar
    Scalar is new advanced OpenAPI UI

--- src/app.py
+++ src/app.py
@@ -3,6 +3,7 @@ import fastapi.middleware
 import fastapi.middleware.cors
 
 import sqladmin
+from scalar_fastapi import get_scalar_api_reference
 
 from config import settings
 
@@ -16,7 +17,6 @@ app = fastapi.FastAPI(redirect_slashes=False)
 app.include_router(http_routers.record_api_router)
 app.include_router(http_routers.user_api_router)
 app.include_router(ws_routers.ws_message_router)
-
 # Middleware settings
 app.add_middleware(
     fastapi.middleware.cors.CORSMiddleware,
@@ -26,6 +26,17 @@ app.add_middleware(
     allow_headers=["*"],
 )
 app.middleware("http")(middleware.authorize)
+
+
+@app.get("/", include_in_schema=False)
+async def scalar_html():
+    return get_scalar_api_reference(
+        # Your OpenAPI document
+        openapi_url=app.openapi_url,
+        # Avoid CORS issues (optional)
+        scalar_proxy_url="https://proxy.scalar.com",
+    )
+
 
 # Admin UI settings
 Admin = sqladmin.Admin(app=app, engine=settings.engine)
